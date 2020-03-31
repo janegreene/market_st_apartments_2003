af16d79766f74c9c533ae68b9791f616ba959f87
Jane Greene <janegreenecasa@gmail.com>
2020-03-31 10:25:46 -0600

    Add add rented test, still not passing, need to come back

--- lib/building.rb
+++ lib/building.rb
@@ -1,12 +1,22 @@
 class Building
-  attr_reader :units
+  attr_reader :units, :renters
 
   def initialize
     @units = []
+    @renters = []
   end
 
   def add_unit(unit)
     @units << unit
   end
+
+  def add_renter(renter_object)
+    require "pry"; binding.pry
+    #@renters << renter_to_add
+  end
+
+  # def average_rent
+  #
+  # end
 
 end
--- test/building_test.rb
+++ test/building_test.rb
@@ -20,26 +20,36 @@ class BuildingTest < Minitest::Test
 
     assert_equal [unit1, unit2], building.units
   end
+
+  def test_it_has_renter
+    building = Building.new
+    assert_equal [], building.renters
+  end
+
+  def test_it_can_add_renter
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    renter1 = Renter.new("Aurora")
+    unit1.add_renter(renter1)
+
+    assert_equal ["Aurora"], building.renters
+
+    renter2 = Renter.new("Tim")
+    unit2.add_renter(renter2)
+    assert_equal ["Aurora", "Tim"] , building.renters
+  end
+
+  # def test_average_rent
+  #   building = Building.new
+  #   unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+  #   unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+  #   building.add_unit(unit1)
+  #   building.add_unit(unit2)
+  #
+  #   assert_equal 1099.5, building.average_rent
+  # end
 end
 
-# pry(main)> building.renters
-# # => []
-#
-# pry(main)> renter1 = Renter.new("Aurora")
-# # => #<Renter:0x00007fa83bc37978...>
-#
-# pry(main)> unit1.add_renter(renter1)
-#
-# pry(main)> building.renters
-# # => ["Aurora"]
-#
-# pry(main)> renter2 = Renter.new("Tim")
-# # => #<Renter:0x00007fa83b9b0358...>
-#
-# pry(main)> unit2.add_renter(renter2)
-#
-# pry(main)> building.renters
-# # => ["Aurora", "Tim"]
 #
 # pry(main)> building.average_rent
 # # => 1099.5
