d864e5d2a2a7045a1087b8bf1872a55fed9adafb
Jane Greene <janegreenecasa@gmail.com>
2020-03-31 09:47:34 -0600

    Added test and method for adding renter

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,10 +1,15 @@
 class Apartment
-  attr_accessor :number
+  attr_accessor :number, :monthly_rent, :bathrooms, :bedrooms, :renter
   def initialize(data)
     @number = data[:number]
     @monthly_rent = data[:monthly_rent]
     @bathrooms = data[:bathrooms]
     @bedrooms = data[:bedrooms]
+    @renter = nil
+  end
+
+  def add_renter(renter)
+    @renter = renter
   end
 
 end
--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -9,5 +9,20 @@ class ApartmentTest < Minitest::Test
     unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
     assert_instance_of Apartment, unit1
     assert_equal "A1", unit1.number
+    assert_equal 1200, unit1.monthly_rent
+    assert_equal 1, unit1.bathrooms
+    assert_equal 1, unit1.bedrooms
+  end
+
+  def test_it_has_renter
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    assert_nil unit1.renter
+  end
+
+  def test_it_can_add_renter
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    renter1 = Renter.new("Jessie")
+    unit1.add_renter(renter1)
+    assert_equal renter1, unit1.renter
   end
 end
